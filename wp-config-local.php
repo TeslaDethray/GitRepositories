59556db12ab384d35c42b2eec263a9d4401af742
TeslaDethray <tesladethray@gmail.com>
2015-05-07 09:40:16 -0700

    Committing for public viewing

---
+++ wp-config-local.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * This config file is yours to hack on. It will work out of the box on Pantheon
+ * but you may find there are a lot of neat tricks to be used here.
+ *
+ * See our documentation for more details:
+ *
+ * http://helpdesk.getpantheon.com/
+ */
+
+/**
+ * Local configuration information.
+ *
+ * If you are working in a local/desktop development environment and want to
+ * keep your config separate, we recommend using a 'wp-config-local.php' file,
+ * which you should also make sure you .gitignore.
+ */
+    define('DB_NAME', 'github');
+
+    /** MySQL database username */
+    define('DB_USER', 'root');
+
+    /** MySQL database password */
+    define('DB_PASSWORD', 'amethyst');
+
+    /** MySQL hostname; on Pantheon this includes a specific port number. */
+    define('DB_HOST', 'localhost');
+
+    /** Database Charset to use in creating database tables. */
+    define('DB_CHARSET', 'utf8');
+
+    /** The Database Collate type. Don't change this if in doubt. */
+    define('DB_COLLATE', '');
